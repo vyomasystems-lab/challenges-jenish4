eeaf1a17faf8fa1f8b997f45dbaefdd1425de514
Jenish Shah <jenishshah08@gmail.com>
2022-07-31 20:21:09 +0000

    edit_0

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,5 +6,5 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
-
+    print("Test Started")
     cocotb.log.info('##### CTB: Develop your test here ########')
